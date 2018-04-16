db46e1c01ac3d5c7d6f112787d11b202f49919a6
armandosegura <segura_arm@yahoo.com>
2018-04-16 15:52:03 -0500

    fixed running error

---
+++ src/main/java/com/example/blog/controllers/HelloWorldController.java
@@ -0,0 +1,31 @@
+package com.example.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+class HelloWorldController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/logout")
+    @ResponseBody
+    public String logout(){
+        return "you have been logged out";
+    }
+
+    @RequestMapping(path = "/lights", method = RequestMethod.GET)
+    @ResponseBody
+    public String lights(){
+        return "lights on";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+}
