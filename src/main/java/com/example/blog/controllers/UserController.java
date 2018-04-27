a299cb4a6a06f7f9e4584ae73d08e8faf4cf71c9
armandosegura <segura_arm@yahoo.com>
2018-04-27 09:29:38 -0500

    need to work on profile page

--- src/main/java/com/example/blog/controllers/UserController.java
+++ src/main/java/com/example/blog/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.example.blog.controllers;
 
+import com.example.blog.models.Post;
 import com.example.blog.models.User;
 import com.example.blog.repositories.UsersRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -33,6 +38,26 @@ public class UserController {
         users.save(user);
         return "redirect:/login";
         }
+
+    @GetMapping("/profile")
+    public String showProfile( Model model) {
+
+//        if (validation.hasErrors()) {
+//            model.addAttribute("errors", validation);
+//            model.addAttribute("user", user);
+//            return "/login";
+//        } else {
+//            User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//            return "redirect:/profile";
+//        }
+
+//
+        User user = new User();
+        model.addAttribute("user", user);
+        return "users/profile";
+    }
+
+
 
         //FIX THIS
 //        @GetMapping("/login")
